64e2f388d09c1564c44039e389b8e370567239ea
scotchfield <swrittenb@gmail.com>
2017-10-15 16:01:09 -0400

    Updates for React 16, bump version number, save JS with prettier

--- src/app.js
+++ src/app.js
@@ -1,58 +1,63 @@
-import React from 'react'
-import { render } from 'react-dom'
-import elasticsearch from 'elasticsearch'
+import React, { Component } from "react";
+import { render } from "react-dom";
+import elasticsearch from "elasticsearch";
 
 let client = new elasticsearch.Client({
-	host: 'localhost:9200',
-	log: 'trace'
-})
+  host: "localhost:9200",
+  log: "trace"
+});
 
-const App = React.createClass({
-	getInitialState () {
-		return {
-			results: []
-		}
-	},
-	handleChange ( event ) {
-		const search_query = event.target.value
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-		client.search({
-			q: search_query
-		}).then(function ( body ) {
-			this.setState({ results: body.hits.hits })
-		}.bind(this), function ( error ) {
-			console.trace( error.message );
-		});
-	},
-	render () {
-		return (
-			<div className="container">
-				<input type="text" onChange={ this.handleChange } />
-				<SearchResults results={ this.state.results } />
-			</div>
-		)
-	}
-})
+    this.state = { results: [] };
+  }
+  handleChange(event) {
+    const search_query = event.target.value;
 
-const SearchResults = React.createClass({
-	propTypes: {
-		results: React.PropTypes.array
-	},
-	getDefaultProps () {
-		return { results: [] }
-	},
-	render () {
-		return (
-			<div className="search_results">
-				<hr />
-				<ul>
-				{ this.props.results.map((result) => {
-					return <li key={ result._id }>{ result._source.name }</li> }) }
-				</ul>
-			</div>
-		)
-	}
-})
+    client
+      .search({
+        q: search_query
+      })
+      .then(
+        function(body) {
+          this.setState({ results: body.hits.hits });
+        }.bind(this),
+        function(error) {
+          console.trace(error.message);
+        }
+      );
+  }
+  render() {
+    return (
+      <div className="container">
+        <input type="text" onChange={this.handleChange} />
+        <SearchResults results={this.state.results} />
+      </div>
+    );
+  }
+}
 
+class SearchResults extends Component {
+  render() {
+    const results = this.props.results || [];
 
-render( <App />, document.getElementById( 'main' ) )
+    return (
+      <div className="search_results">
+        <hr />
+        <ul>
+          {results.map(result => {
+            return (
+              <li key={result._id}>
+                {result._source.name}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("main"));
